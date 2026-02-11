4f66b5670cdb139b57bd77dd8b44b21c57fa34ca
Hamish <79484478+Mish-Cube@users.noreply.github.com>
2026-02-11 14:20:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 var map = L.map('map').setView([20, 0], 2);
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
-    subdomains: 'abcd',
-    maxZoom: 19,
-    noWrap: true
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '© OpenStreetMap'
 }).addTo(map);
 map.setMaxBounds([
-  [-85, -180], // Southwest corner
-  [85, 180]    // Northeast corner
+  [-85, -180],
+  [85, 180]
 ]);
 
 // Example location
@@ -16,14 +13,16 @@ var london = `
 <h2>London</h2>
 <p>The truth is, that in London it is always a sickly season. Nobody is healthy in London, nobody can be. –Jane Austen</p>
 
-<img src="london1.jpg">
-<img src="london2.jpg">
-<img src="london3.jpg">
-<img src="london4.jpg">
-<img src="london5.jpg">
-<img src="london6.jpg">
-<img src="london7.jpg">
-<img src="london8.jpg">
+<div class="popup-gallery">
+    <img src="london1.jpg">
+    <img src="london2.jpg">
+    <img src="london3.jpg">
+    <img src="london4.jpg">
+    <img src="london5.jpg">
+    <img src="london6.jpg">
+    <img src="london7.jpg">
+    <img src="london8.jpg">
+</div>
 
 <audio controls>
   <source src="londonvoice.mp3" type="audio/mpeg">

d9d53df270d2ff9edce84c64f50bab8f8baaed49
Hamish <79484478+Mish-Cube@users.noreply.github.com>
2026-02-11 15:37:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -18,31 +18,75 @@ var cameraIcon = L.icon({
 });
 
 
+var locations = [
+  {
+    name: "London",
+    coords: [51.5074, -0.1278],
+    photos: [
+      "london1.jpg",
+      "london2.jpg",
+      "london3.jpg",
+      "london4.jpeg",
+      "london5.jpeg",
+      "london6.png",
+      "london7.jpg",
+      "london8.jpg"
+    ],
+    audio: "londonvoice.mp3",
+    text: "London, that great cesspool into which all the loungers and idlers of the Empire are irresistibly drained. –Sir Arthur Conan Doyle"
+  },
 
 
-var london = `
-<h2>London</h2>
-<p>London, that great cesspool into which all the loungers and idlers of the Empire are irresistibly drained. –Sir Arthur Conan Doyle</p>
+  ];
 
-<div class="popup-gallery">
-    <img src="london1.jpg">
-    <img src="london2.jpg">
-    <img src="london3.jpg">
-    <img src="london4.jpeg">
-    <img src="london5.jpeg">
-    <img src="london6.png">
-    <img src="london7.jpg">
-    <img src="london8.jpg">
-</div>
 
-<audio controls>
-  <source src="londonvoice.mp3" type="audio/mpeg">
-</audio>
-`;
 
-L.marker([51.5074, -0.1278], { icon: cameraIcon })
-  .addTo(map)
-  .bindPopup(london, {
-    maxWidth: 900,  // or whatever width you like
+locations.forEach((loc, i) => {
+  var popupHTML = `
+    <h2>${loc.name}</h2>
+    <p>${loc.text}</p>
+
+    <div class="popup-slider" data-index="0" data-id="${i}">
+      <button class="prev">&#10094;</button>
+      <img src="${loc.photos[0]}">
+      <button class="next">&#10095;</button>
+    </div>
+
+    <audio controls>
+      <source src="${loc.audio}">
+    </audio>
+  `;
+
+  L.marker(loc.coords).addTo(map).bindPopup(popupHTML, {
+    maxWidth: 900,
     minWidth: 600
   });
+});
+
+
+
+map.on("popupopen", function(e) {
+  var popup = e.popup.getElement();
+  if (!popup) return;
+
+  var slider = popup.querySelector(".popup-slider");
+  if (!slider) return;
+
+  var id = slider.dataset.id;
+  var img = slider.querySelector("img");
+  var prev = slider.querySelector(".prev");
+  var next = slider.querySelector(".next");
+
+  var photos = locations[id].photos;
+  var index = 0;
+
+  prev.onclick = function() {
+    index = (index - 1 + photos.length) % photos.length;
+    img.src = photos[index];
+  };
+
+  next.onclick = function() {
+    index = (index + 1) % photos.length;
+    img.src = photos[index];
+  };
+});
